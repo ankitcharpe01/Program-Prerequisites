3eb4bf6203d8d9fc115063bb5455c2b7dc84bb47
ankitcharpe01 <ankitcharpe01@gmail.com>
2025-01-08 11:40:36 +0530

    addTwoNumbers to AddTwoNumbers is updated

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class addTwoNumbers {
+public class AddTwoNumbers {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 	 	System.out.println("Enter First Number");
